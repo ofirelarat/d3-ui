38d033e294e7aa34d374c1e82346d1328173667f
ofirelarat <96ofir11@gmail.com>
2025-10-14 11:48:00 +0300

    try

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -1,15 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@d3-ui': process.cwd() + '/../components',
-    };
-    return config;
-  },
   experimental: {
-    externalDir: true, // Allow importing components from outside the app directory
+    externalDir: true // Allow importing components from outside the app directory
   }
 };
 
