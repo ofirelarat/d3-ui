8989d6cff986cb2fa94bc93b492f6f2cc30ba6bd
ofirelarat <96ofir11@gmail.com>
2025-10-14 11:50:59 +0300

    fix alias

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@d3-ui': process.cwd() + '/../components',
+    };
+    return config;
+  },
   experimental: {
-    externalDir: true // Allow importing components from outside the app directory
+    externalDir: true, // Allow importing components from outside the app directory
   }
 };
 
